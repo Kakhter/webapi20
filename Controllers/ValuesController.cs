96143eee2339792962bae1f351a4b7d14d35eaf6
Kakhter <khalid.bharat@gmail.com>
2021-11-15 04:20:18 -0800

    Update ValuesController.cs

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace webapi20.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "Name", "Khalid a" };
+            return new string[] { "Name", "Khalid ak" };
         }
 
         // GET api/values/5
